fd74b42115906260e16845a9b74fa9cdae0b2fd6
shehrozraza <shehroz.raza11@hotmail.com>
2024-03-17 16:39:59 +0500

    login page functionality

--- login.js
+++ login.js
@@ -1,19 +1,30 @@
 function myData(){
-    // let email = document.getElementById("email").value
-    // let password = document.getElementById("password").value
-    // console.log(email);
-    // console.log(password);
 
-    // if (email == "shehroz.raza11@hotmail.com" && password == "12345678"){
-    //     alert("login successful")
-    // }else{
-    //     alert("login failed try again")
-    // }
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
 
-   fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(data => console.log("data",data[499].email))
-    
+    if (email == "shehroz.raza11@hotmail.com" && password == "12345678"){
+        localStorage.setItem("email",email)
+        localStorage.setItem("password",password)
+        alert("login successful")
+        document.getElementById("password").value=""
+        document.getElementById("email").value=""
+    }else{
+        localStorage.removeItem("email")
+        localStorage.removeItem("password")
+        alert("login failed try again")
+       
+    }
+
+//    fetch('https://jsonplaceholder.typicode.com/albums')
+//     .then(response => response.json())
+//     // console.log("data",data[0].email)
+//     .then(data =>{
+//         let email=data[0].email
+       
+//         let ls = localStorage.getItem("data",JSON.stringify(data)) 
+//         console.log(ls);
+//     } )
     
    
 
